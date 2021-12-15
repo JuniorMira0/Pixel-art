572efb702e56ad72f2e5c58f3a6aedbda27b3e80
JuniorMira <j-unyor14@hotmail.com>
2021-12-15 14:27:42 -0300

    criado a paleta de cores e os block pixels

---
+++ script.js
@@ -0,0 +1,21 @@
+// making a corlor palette
+function makeColorBlock(element) {
+  for (let i = 0; i < 4; i += 1) {
+    const colorPalette = document.getElementById('color-palette');
+    const newDiv = document.createElement(element);
+    newDiv.className = 'color';
+    colorPalette.appendChild(newDiv);
+  }
+}
+function makeBoardPixel() {
+  for (let i = 0; i < 25; i += 1) {
+    const pixelBoard = document.getElementById('pixel-board');
+    const pixelBlock = document.createElement('div');
+    pixelBlock.className = 'pixel';
+    pixelBoard.appendChild(pixelBlock);
+  }
+}
+window.onload = () => {
+  makeColorBlock('div');
+  makeBoardPixel();
+};
