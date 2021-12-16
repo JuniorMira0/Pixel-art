ea0cebc28ecec524a68ed26b563e5c7707c465ff
JuniorMira <j-unyor14@hotmail.com>
2021-12-15 21:47:17 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // making a corlor palette
 function makeColorBlock(element) {
-  for (let i = 0; i < 4; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const colorPalette = document.getElementById('color-palette');
     const newDiv = document.createElement(element);
     newDiv.className = 'color';
@@ -16,8 +16,31 @@ function makeBoardPixel() {
     pixelBoard.appendChild(pixelBlock);
   }
 }
+// seleciona as cores
+function selectColors() {
+  const selectedColor = document.getElementsByClassName('color');
+  for (let i = 0; i < selectedColor.length; i += 1) {
+    selectedColor[i].addEventListener('click', () => {
+      selectedColor[i].classList.add('selected');
+    });
+  }
+}
+/*
+Passar pelas 4 cores, verificar se existe algum selected na lista de classes e se tiver ela apaga a classe
+*/
+
+function clearClass() {
+  const selectedColor = document.getElementsByClassName('color');
+  for (let i = 0; i < selectedColor.length; i += 1) {
+    if (selectedColor[i].contains('selected')) {
+      selectedColor[i].classList.remove();
+    }
+  }
+}
 
 window.onload = () => {
   makeColorBlock('div');
   makeBoardPixel();
+  selectColors();
+  clearClass();
 };
