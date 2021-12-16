2cdf1bfd707de8f681e35052c7cf15c74907fde7
JuniorMira <j-unyor14@hotmail.com>
2021-12-16 00:12:43 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // making a corlor palette
-function makeColorBlock(element) {
+function makeColorBlock() {
   for (let i = 0; i < 3; i += 1) {
     const colorPalette = document.getElementById('color-palette');
-    const newDiv = document.createElement(element);
+    const newDiv = document.createElement('div');
     newDiv.className = 'color';
     colorPalette.appendChild(newDiv);
   }
@@ -17,30 +17,31 @@ function makeBoardPixel() {
   }
 }
 // seleciona as cores
-function selectColors() {
-  const selectedColor = document.getElementsByClassName('color');
-  for (let i = 0; i < selectedColor.length; i += 1) {
-    selectedColor[i].addEventListener('click', () => {
-      selectedColor[i].classList.add('selected');
-    });
-  }
-}
-/*
-Passar pelas 4 cores, verificar se existe algum selected na lista de classes e se tiver ela apaga a classe
-*/
-
 function clearClass() {
   const selectedColor = document.getElementsByClassName('color');
   for (let i = 0; i < selectedColor.length; i += 1) {
-    if (selectedColor[i].contains('selected')) {
-      selectedColor[i].classList.remove();
+    if (selectedColor[i].classList.contains('selected')) {
+      selectedColor[i].classList.remove('selected');
     }
   }
 }
 
+function selectColors() {
+  const selectedColor = document.getElementsByClassName('color');
+  for (let i = 0; i < selectedColor.length; i += 1) {
+    selectedColor[i].addEventListener('click', () => {
+      clearClass();
+      selectedColor[i].classList.add('selected');
+    });
+  }
+}
+
+/*
+Passar pelas 4 cores, verificar se existe algum selected na lista de classes e se tiver ela apaga a classe
+*/
+
 window.onload = () => {
-  makeColorBlock('div');
+  makeColorBlock();
   makeBoardPixel();
   selectColors();
-  clearClass();
 };
